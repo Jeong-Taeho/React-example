99b6ff6555e52bf1cf617ef433dd21428f85bf26
Jeong-Taeho <nabi218218@gmail.com>
2023-06-14 01:59:36 +0900

    feat : 모달 창 추가

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import { useState } from 'react';
 
 function App() { //jsx임 js파일에서 쓰는 html 대용품
   
-  let [title,change] = useState(['남자 코트 추천','여자 코트 추천','신상품'])
-  let [good, good변경] = useState(0)
+  let [title,change] = useState(['남자 코트 추천','여자 코트 추천','신상품']);
+  let [good, good변경] = useState(0);
   // a는 보관했던 자료
   // b는 state 변경을 도와주는 함수
   // 변동 시 자동으로 html에 반영되게 만들고 싶으면 state
 
   // state 변경하는 법 : 등호x 
 
+  let [modal, setModal] = useState(false);
+  let countModal = 1;
   return (
     <div className="App">
         <div className ='black-nav'>
@@ -22,14 +24,15 @@ function App() { //jsx임 js파일에서 쓰는 html 대용품
         <button onClick={ () => {
           let copy = [...title];
           copy[0] = '여자 패딩 추천';
-          change(copy);
+          chan
+          ge(copy);
         }}>click</button>
 
         <button onClick={ () => {
           let sorted = [...title].sort();
           change(sorted)
         }}> 정렬 </button>
-
+G
         <div className='list'>
           <h4>{title[0]} <span onClick={() => good변경(good+1) }>👍</span>{good}</h4>
           <p>06월 09일 발행</p>
@@ -41,13 +44,32 @@ function App() { //jsx임 js파일에서 쓰는 html 대용품
         </div>
 
         <div className='list'>
-          <h4>{title[2]} <span onClick={() => good변경(good+1) }>👍</span>{good}</h4>
-          <p>06월 09일 발행</p>
+          <h4 onClick={() => {
+             countModal += 1;
+             console.log(countModal);
+             countModal % 2 === 0 ? setModal(true) : setModal(false)
+             }}> {title[2]} </h4>
+              <p>06월 09일 발행</p>
         </div>
 
+       {
+          modal == true ? <Modal/> : null
+       }   
         
     </div>
   );
+}
+
+function Modal(){
+  return(
+    <>
+      <div className='modal'>
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+    </>
+  )
 }
 
 export default App;
